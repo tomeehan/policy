76d8550fb3cc34cf8e2b6eb44448be6d02a3d42c
Tom Meehan <tom.meehan@me.com>
2026-01-03 12:42:19 +0000

    Add rspec-rails gem and configuration
    - Add rspec-rails to Gemfile
    - Configure rails_helper with fixtures, Devise helpers, WebMock
    - Use existing test/fixtures for fixture data
    - Add json_response and switch_account helpers

---
+++ spec/rails_helper.rb
@@ -0,0 +1,87 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+# Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
+# that will avoid rails generators crashing because migrations haven't been run yet
+# return unless Rails.env.test?
+require 'rspec/rails'
+require 'webmock/rspec'
+# Add additional requires below this line. Rails is not loaded until this point!
+
+SolidQueue.logger.level = Logger::WARN
+
+# Generate a random password so Chrome doesn't warn about passwords in data breaches
+UNIQUE_PASSWORD = Devise.friendly_token
+
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
+# run as spec files by default. This means that files in spec/support that end
+# in _spec.rb will both be required and run as specs, causing the specs to be
+# run twice. It is recommended that you do not name files matching this glob to
+# end with _spec.rb. You can configure this pattern with the --pattern
+# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
+#
+# The following line is provided for convenience purposes. It has the downside
+# of increasing the boot-up time by auto-requiring all files in the support
+# directory. Alternatively, in the individual `*_spec.rb` files, manually
+# require only the support files necessary.
+#
+# Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
+
+# Ensures that the test database schema matches the current schema file.
+# If there are pending migrations it will invoke `db:test:prepare` to
+# recreate the test database by loading the schema.
+# If you are not using ActiveRecord, you can remove these lines.
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+RSpec.configure do |config|
+  # Use existing test fixtures
+  config.fixture_paths = [
+    Rails.root.join('test/fixtures')
+  ]
+
+  config.use_transactional_fixtures = true
+
+  # Infer spec type from file location
+  config.infer_spec_type_from_file_location!
+
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+
+  # Include Devise test helpers for request specs
+  config.include Devise::Test::IntegrationHelpers, type: :request
+
+  # Include fixtures
+  config.global_fixtures = :all
+end
+
+# WebMock configuration
+WebMock.disable_net_connect!(
+  allow_localhost: true,
+  allow: [
+    "chromedriver.storage.googleapis.com",
+    "rails-app",
+    "selenium"
+  ]
+)
+
+# Helper methods
+module RequestSpecHelpers
+  def json_response
+    JSON.parse(response.body)
+  end
+
+  def switch_account(account)
+    patch "/accounts/#{account.id}/switch"
+  end
+end
+
+RSpec.configure do |config|
+  config.include RequestSpecHelpers, type: :request
+end
