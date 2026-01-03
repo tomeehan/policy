477b6c2e2a25f4a01ea5ca2e22670f19c1c7ac0f
Tom Meehan <tom.meehan@me.com>
2026-01-03 15:55:34 +0000

    Remove test/ directory and move fixtures to spec/
    - Move fixtures from test/fixtures to spec/fixtures
    - Move file fixtures from test/fixtures/files to spec/fixtures/files
    - Move mailer previews from test/mailers/previews to spec/mailers/previews
    - Move trix helper from test/support/system to spec/support
    - Delete all minitest files from test/ directory
    - Update CI workflow to use rspec instead of minitest

--- test/integration/heroku_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class Jumpstart::HerokuTest < ActiveSupport::TestCase
-  def test_valid_app_json_syntax
-    assert_nothing_raised do
-      JSON.parse(File.read("app.json"))
-    end
-  end
-end
